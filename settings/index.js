9fd1f8bfadad767916b272cd3146ec30de33d7c4
PironP <piron.pierre@gmail.com>
2018-03-16 10:53:22 +0100

    config heroku

--- settings/index.js
+++ settings/index.js
@@ -1,3 +1,6 @@
 module.exports = server => {
     server.settings = require('./settings.json');
+
+    server.settings.PORT = process.env.PORT || server.settings.PORT;
+    server.settings.db_uri = process.env.MONGO_DB_URI || server.settings.db_uri;
 };
